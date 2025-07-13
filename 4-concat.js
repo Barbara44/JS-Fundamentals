96814deb527918ca834ebdac26262ee8b7bf9edb
barbara nunoo <barbaranunoo00@gmail.com>
2025-07-13 05:34:26 +0000

    Add updated concat script with correct output rules

--- 4-concat.js
+++ 4-concat.js
@@ -1,4 +1,9 @@
 const arg1 = process.argv[2];
 const arg2 = process.argv[3];
 
-console.log(` ${arg1} is ${arg2}`);
+if (arg1 && arg2) {
+    console.log(`${arg1} is ${arg2}`);
+} else if (arg1) {
+    console.log(arg1);
+}
+//If no arguments  do nothing
